b1c597d9f66362137d3708d77f15beb1e2dbd179
Chase Helgenlechner <chase@helgenlechner.com>
2023-04-17 15:09:38 +0200

    First test written for Euler Problem 001.

---
+++ ProjectEuler.Tests/001__Multiples_of_3_or_5_Tests/SummerOf3sAnd5sTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using ProjectEuler._001__Multiples_of_3_or_5;
+
+namespace ProjectEuler.Tests._001__Multiples_of_3_or_5_Tests
+{
+    [TestFixture]
+    public class SummerOf3sAnd5sTests
+    {
+        //The class being tested:
+        SummerOf3sAnd5s _beingTested;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _beingTested = new SummerOf3sAnd5s();
+        }
+
+        [Test]
+        public void Test01()
+        {
+            // arrange (could get pretty big)
+
+            // act (should be a single line or call)
+            int a = _beingTested.FindSumBelowExcluding(10);
+
+            // assert
+            a.Should().Be(23);
+        }
+
+    }
+}
---
+++ ProjectEuler/001__Multiples_of_3_or_5/SummerOf3sAnd5s.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectEuler._001__Multiples_of_3_or_5
+{
+    public class SummerOf3sAnd5s
+    {
+        public int FindSumBelowExcluding(int upperLimit)
+        {
+            return 23;
+        }
+    }
+}
