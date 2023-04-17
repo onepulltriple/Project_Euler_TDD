4f111b4a8c643ff7d6e0c57f79ee4dcc4b036187
Chase Helgenlechner <chase@helgenlechner.com>
2023-04-17 13:34:21 +0200

    Set up project and corresponding test-project with nuget packages.

---
+++ ProjectEuler.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ProjectEuler.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
