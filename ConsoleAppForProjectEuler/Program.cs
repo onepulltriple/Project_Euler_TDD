09f8f4d492cd47fb1c3fe6d4964c92e66d44bc72
Chase Helgenlechner <chase@helgenlechner.com>
2023-04-17 16:20:28 +0200

    Added Console App to organize calling of methods which solve the Euler Project problems.

---
+++ ConsoleAppForProjectEuler/Program.cs
@@ -0,0 +1,23 @@
+﻿using ProjectEuler._001__Multiples_of_3_or_5;
+
+namespace ConsoleAppForProjectEuler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem001();
+        }
+
+        //////////////////////////////////////////////////////////////////////
+        private static void Problem001()
+        {
+            // Problem 001
+            int a = new SummerOf3sAnd5s().FindSumBelowExcluding(1000);
+            Console.WriteLine($"The answer to Problem 1 is: {a}");
+            Console.WriteLine();
+            // Answered correctly 2023.04.17
+            // Answer = 233168
+        }
+    }
+}
