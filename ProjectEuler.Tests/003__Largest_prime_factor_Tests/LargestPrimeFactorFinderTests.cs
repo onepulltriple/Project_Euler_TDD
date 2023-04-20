c2167d2639bc4ea2c46a1ba9bc896afd2abf1496
Chase Helgenlechner <chase@helgenlechner.com>
2023-04-20 13:46:00 +0200

    Testing setup for Euler Problem 3 completed.

---
+++ ProjectEuler.Tests/003__Largest_prime_factor_Tests/LargestPrimeFactorFinderTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using ProjectEuler._003__Largest_prime_factor;
+
+namespace ProjectEuler.Tests._003__Largest_prime_factor_Tests
+{
+    [TestFixture]
+    public class LargestPrimeFactorFinderTests
+    {
+        // The class being tested.
+        LargestPrimeFactorFinder _underTest;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _underTest = new();
+        }
+
+        [Test]
+        public void _00__ProjectEulerExampleTest()
+        {
+            // arrange
+            int compositeNumber = 13195;
+            int largestPrimeFactor = 29;
+
+            // act
+            int a = _underTest.FindLargestPrimeFactor(compositeNumber);
+
+            // assert
+            a.Should().Be(largestPrimeFactor);
+
+
+        }
+    }
+}
---
+++ ProjectEuler/003__Largest_prime_factor/LargestPrimeFactorFinder.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectEuler._003__Largest_prime_factor
+{
+    public class LargestPrimeFactorFinder
+    {
+        public int FindLargestPrimeFactor(int compositeNumber)
+        {
+            int temp = 29;
+            // check that input parameter is really a composite number
+
+            return temp;
+        }
+    }
+}
